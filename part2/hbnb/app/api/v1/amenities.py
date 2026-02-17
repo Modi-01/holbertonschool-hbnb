d68697b3d361d775961eef5dc71ea547e8e3bf5b
Randa-hb10 <rm-400000@hotmail.com>
2026-02-17 09:46:11 +0300

    Remove AmenitiesCollection class and its methods

--- part2/hbnb/app/api/v1/amenities.py
+++ part2/hbnb/app/api/v1/amenities.py
@@ -1,15 +1,7 @@
+#!/usr/bin/python3
 from flask_restx import Namespace, Resource
+from flask import request
+
 from app.services import facade
 
 ns = Namespace("amenities", description="Amenity operations")
-
-
-@ns.route("/")
-class AmenitiesCollection(Resource):
-    def get(self):
-        
-        return {"message": "GET /amenities"}, 200
-
-    def post(self):
-        
-        return {"message": "POST /amenities"}, 201
