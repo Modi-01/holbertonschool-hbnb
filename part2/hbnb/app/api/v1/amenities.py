6a4216097d272754f765ddadf6c7bb9066df84c8
Randa-hb10 <rm-400000@hotmail.com>
2026-02-17 09:46:46 +0300

    Implement GET method for amenities collection
    Add GET endpoint to retrieve list of amenities.

--- part2/hbnb/app/api/v1/amenities.py
+++ part2/hbnb/app/api/v1/amenities.py
@@ -5,3 +5,13 @@ from flask import request
 from app.services import facade
 
 ns = Namespace("amenities", description="Amenity operations")
+
+@ns.route("/")
+class AmenitiesCollection(Resource):
+    def get(self):
+        """
+        GET /api/v1/amenities/
+        Return list of amenities
+        """
+        amenities = facade.amenities.list_all()
+        return [a.to_dict() for a in amenities], 200
