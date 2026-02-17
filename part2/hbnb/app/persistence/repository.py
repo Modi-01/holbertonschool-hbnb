5cca51528745913192b79653046c3406eecd6483
Modi-01 <moudhi.a.almutlaq@gmail.com>
2026-02-17 08:45:55 +0300

    Update repository.py

--- part2/hbnb/app/persistence/repository.py
+++ part2/hbnb/app/persistence/repository.py
@@ -1,85 +1,20 @@
-from abc import ABC, abstractmethod
+#!/usr/bin/python3
 
-
-class Repository(ABC):
-    """
-    Repository interface (Persistence Layer).
-    
-    """
-
-    @abstractmethod
-    def add(self, obj):
-        pass
-
-    @abstractmethod
-    def get(self, obj_id):
-        pass
-
-    @abstractmethod
-    def get_all(self):
-        pass
-
-    @abstractmethod
-    def update(self, obj_id, data):
-        pass
-
-    @abstractmethod
-    def delete(self, obj_id):
-        pass
-
-    @abstractmethod
-    def get_by_attribute(self, attr_name, attr_value):
-        pass
-
-
-class InMemoryRepository(Repository):
-    """
-    Simple in-memory repository.
-    """
+class InMemoryRepository:
+    """Store objects by id in memory."""
 
     def __init__(self):
-        self._storage = {}
-
-    def _validate_object(self, obj):
-        """
-        Minimal validation to keep the repository consistent.
-        """
-        if obj is None:
-            raise ValueError("Cannot store a None object.")
-
-        if not hasattr(obj, "id"):
-            raise ValueError("Stored objects must have an 'id' attribute.")
-
-        if obj.id is None:
-            raise ValueError("Object 'id' must not be None.")
+        self._data = {}
 
     def add(self, obj):
-        self._validate_object(obj)
-        self._storage[obj.id] = obj
-
-    def get(self, obj_id):
-        return self._storage.get(obj_id)
-
-    def get_all(self):
-        return list(self._storage.values())
-
-    def update(self, obj_id, data):
-        
-        obj = self.get(obj_id)
-        if obj and isinstance(data, dict):
-            for key, value in data.items():
-                setattr(obj, key, value)
-            self._storage[obj_id] = obj
+        self._data[obj.id] = obj
         return obj
 
-    def delete(self, obj_id):
-        if obj_id in self._storage:
-            del self._storage[obj_id]
+    def get(self, obj_id):
+        return self._data.get(obj_id)
 
-    def get_by_attribute(self, attr_name, attr_value):
-        
-        return next(
-            (obj for obj in self._storage.values()
-             if hasattr(obj, attr_name) and getattr(obj, attr_name) == attr_value),
-            None
-        )
+    def delete(self, obj_id):
+        return self._data.pop(obj_id, None)
+
+    def list_all(self):
+        return list(self._data.values())
