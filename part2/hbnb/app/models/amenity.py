f7cd91a5b201cd8d6607873cc1aec809d8288905
Randa-hb10 <rm-400000@hotmail.com>
2026-02-21 04:38:27 +0300

    Refactor set_name method in amenity.py
    Refactor set_name method to remove autosave parameter and add length check for name.

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -8,23 +8,27 @@ class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
         self.name = None
-        self.places = []
-        self.set_name(name, autosave=False)
+        self.set_name(name)
+
+
+    def set_name(self, name):
+        name = name.strip()
+        print('leng')
+        print(len(name))
+        print(name)
+
+        if len(name) > 50:
+            return {"error": "Name must be less than 50 characters."}
+        
+        self.name = name
         self.save()
 
-
-    def set_name(self, name, autosave=True):
-        if not isinstance(name, str) or not name.strip():
-            raise ValueError("name is required and must be a non-empty string")
-        name = name.strip()
-        if len(name) > 50:
-            raise ValueError("name must not exceed 50 characters")
-        self.name = name
-        if autosave:
-            self.save()
-
     def update_amenity(self, data):
-        if not isinstance(data, dict):
-            raise ValueError("data must be a dict")
-        if "name" in data:
-            self.set_name(data["name"])
+        self.update(data)
+
+    # Define return fields
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        } 
