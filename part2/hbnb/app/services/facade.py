b51481173375ab71be616b4942dfe61102eeea5c
your0T0 <yara241020@gmail.com>
2026-02-21 12:12:08 +0000

    Add amenity management methods to HbnBFacade (create, list, get, update)

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -44,3 +44,25 @@ class HBnBFacade:
 
     # -------- End of User Methods --------
 
+    # -------- Amenity Methods --------
+
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(name=amenity_data.get('name'))
+        
+        return self.amenity_repo.add(amenity)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def update_amenity(self, amenity_id, data):
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            raise ValueError("amenity not found")
+        
+        amenity.update_amenity(data)
+        return amenity
+    
+    # -------- End of Amenity Methods --------
