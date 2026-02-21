e619ed2f860e2584efb8c52405cd0fc93c58be97
your0T0 <yara241020@gmail.com>
2026-02-21 12:09:54 +0000

    Add user management methods to HbnBFacade (create, list, get, update)

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -13,3 +13,34 @@ class HBnBFacade:
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
+
+    # -------- User Methods --------
+
+    def create_user(self, user_data):
+        user = User(
+            email=user_data['email'],
+            first_name=user_data['first_name'],
+            last_name=user_data['last_name']
+        )
+
+        return self.user_repo.add(user)
+
+    def list_users(self):
+        return self.user_repo.get_all()
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    def update_user(self, user_id, data):
+        user = self.user_repo.get(user_id)
+        if not user:
+            raise ValueError("user not found")
+
+        user.update_user(data)
+        return user
+
+    # -------- End of User Methods --------
+
