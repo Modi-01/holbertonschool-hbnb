46b52e8cb51044af7e15e0864c3b25740dee5af5
your0T0 <yara241020@gmail.com>
2026-02-21 12:19:24 +0000

    Add review CRUD with validation

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -122,3 +122,59 @@ class HBnBFacade:
 
 
     # -------- End of Place Methods --------
+
+    # -------- Review Methods --------
+    def create_review(self, review_data):
+        if not isinstance(review_data['text'], str) or not review_data['text'].strip():
+            raise ValueError("text is required and must be a non-empty string")
+
+        author = self.user_repo.get(review_data['user_id'])
+        if not isinstance(author, User) or not author:
+            raise ValueError("user couldn't be found")
+
+        place = self.place_repo.get(review_data['place_id'])
+        if not isinstance(place, Place) or not place:
+            raise ValueError("place must exist")
+
+        for review in place.reviews:
+            if(review.author == author):
+                raise ValueError("You already rated the place. You can edit your rating if you like. Rating id" + review.id)
+
+        review = Review(text = review_data['text'], rating = review_data['rating'], author = author, place = place)
+        self.review_repo.add(review)
+        place.add_review(review)
+
+        return review
+
+    def get_all_reviews(self):
+        return self.review_repo.get_all()
+
+    def get_review(self, review_id):
+        return self.review_repo.get(review_id)
+
+    def update_review(self, review_id, data):
+        review = self.review_repo.get(review_id)
+        if not isinstance(review, Review) or not review:
+            raise ValueError("review not found")
+
+        review.update_review(data)
+
+        return review
+
+    def delete_review(self, review_id):
+        review = self.review_repo.get(review_id)
+        if not isinstance(review, Review) or not review:
+            raise ValueError("review not found")
+
+        place = review.place
+        if not isinstance(place, Place) or not place:
+            raise ValueError("place must exist")
+
+        review_obj = next((r for r in place.reviews if r.id == review_id), None)
+
+        if review_obj:
+            place.reviews.remove(review_obj)
+
+        self.review_repo.delete(review_id)
+     
+     # -------- End of Place Methods --------
